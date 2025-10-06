362f9329518571b8603cf391919878089ac79afa
leenalab <leenaleelab@gmail.com>
2025-10-06 14:11:21 +0300

    functions 2

--- functions 2.py
+++ functions 2.py
@@ -2,5 +2,8 @@ def format_string(string, length):
     if len(string) >= length:
         return string
     else:
-        spaces = (length - len(string)) // 2
-        return " " * spaces + string
+        total_spaces = length - len(string)
+        left_spaces = total_spaces // 2
+        right_spaces = total_spaces - left_spaces
+        return " " * left_spaces + string + " " * right_spaces
+print(format_string("Hi", 4))
