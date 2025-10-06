5246809b906aaf04c8101db9b2fd86806514544e
leenalab <leenaleelab@gmail.com>
2025-10-06 14:34:31 +0300

    functions 2

--- functions 2.py
+++ functions 2.py
@@ -1,9 +1,11 @@
-def format_string(string, length):
-    if len(string) >= length:
-        return string
+def factorial(n):
+    print("Виклик функції factorial з n = ", n)
+    if n == 1:
+        print("Базовий випадок, n = 1, повернення 1")
+        return 1
     else:
-        total_spaces = length - len(string)
-        left_spaces = total_spaces // 2
-        right_spaces = total_spaces - left_spaces
-        return " " * left_spaces + string + " " * right_spaces
-print(format_string("Hi", 4))
+        result = n * factorial(n-1)
+        print("Повернення результату для n = ", n, ": ", result)
+        return result
+
+print(factorial(5))
