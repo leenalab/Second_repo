ef50e63cc9fbef74ef112505c6f465056c0e1fcd
leenalab <leenaleelab@gmail.com>
2025-10-06 14:59:34 +0300

    functions 2

--- functions 2.py
+++ functions 2.py
@@ -1,11 +1,13 @@
 def factorial(n):
-    print("Виклик функції factorial з n = ", n)
-    if n == 1:
-        print("Базовий випадок, n = 1, повернення 1")
+    if n < 2:
         return 1
     else:
-        result = n * factorial(n-1)
-        print("Повернення результату для n = ", n, ": ", result)
-        return result
+        return n * factorial(n - 1)
 
-print(factorial(5))
+
+def number_of_groups(n, k):
+    return factorial(n) // (factorial(k)*factorial(n-k))
+
+
+
+print(number_of_groups(50, 7))
