a7f8be56d2d9407ea9ac80b6a36d98f46d14e457
leenalab <leenaleelab@gmail.com>
2025-10-09 21:09:18 +0300

    voices

--- regex.py
+++ regex.py
@@ -1,2 +1,12 @@
 # regEx regular expression
 # for what: search, validation, split, sub
+import re
+
+re.search #search substr in str
+re.match #search substr in str
+re.sub #
+re.split
+re.findall
+
+res=re.findall(r"[aeiou]+", "vsadlchöaskjchöaschöaKJHCY") #['e', 'o', 'o']
+print(res)
