ce51c61890993aafc7b4a0d6ac665e2d77b6a3c2
leenalab <leenaleelab@gmail.com>
2025-10-12 20:23:40 +0300

    ff

--- daytime.py
+++ daytime.py
@@ -1,1 +1,15 @@
 import datetime
+
+now = datetime.datetime.now()
+print(now)
+from datetime import datetime
+
+current_datetime = datetime.now()   
+print(current_datetime.year)
+print(current_datetime.month)
+print(current_datetime.day)
+print(current_datetime.hour)
+print(current_datetime.minute)
+print(current_datetime.second)
+print(current_datetime.microsecond)
+print(current_datetime.tzinfo)
