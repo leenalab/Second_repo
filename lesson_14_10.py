667fcd0d807cfd7e6a453ce59838f7c32693e633
leenalab <leenaleelab@gmail.com>
2025-10-15 23:43:00 +0300

    How are you

--- lesson_14_10.py
+++ lesson_14_10.py
@@ -1,10 +1,13 @@
 fh = open('names.txt', 'w')
-fh.write('hello!')
+fh.write('How are you!')
 fh.close()
 
 fh = open('names.txt', 'r')
-all_file = fh.read()
-print(all_file)  # 'hello!'
+while True:
+    symbol = fh.read(1)
+    if len(symbol) == 0:
+        break
+    print(symbol)
 
 fh.close()
 
