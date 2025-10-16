4d520a07947b9b4512e5f3b06b69e79d3a136e92
leenalab <leenaleelab@gmail.com>
2025-10-16 19:37:04 +0300

    first line

--- lesson_14_10.py
+++ lesson_14_10.py
@@ -1,14 +1,10 @@
-fh = open('names.txt', 'w')
-fh.write('How are you!')
+fh = open("names.txt", "w")
+fh.write("first line\nsecond line\nthird line")
 fh.close()
 
-fh = open('names.txt', 'r')
-while True:
-    symbol = fh.read(1)
-    if len(symbol) == 0:
-        break
-    print(symbol)
+fh = open("names.txt", "r")
+lines = [el.strip() for el in fh.readlines()]
+print(lines)
 
 fh.close()
-
 
